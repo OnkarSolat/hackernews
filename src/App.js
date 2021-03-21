13b54499a84033556c2a7693142907bb2e160d27
Onkar <omsolat16@gmail.com>
2021-03-21 23:38:45 +0530

    EOD Hacker news

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector } from 'react-redux'
+import Header from "./Components/Header";
+import SearchBar from './Components/SearchBar'; 
+import Result from './Components/Result';
+// import {useState} from 'react';
+
 
 function App() {
+  // const [results, setResults] = useState("");
+  const results = useSelector(state => state.results)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Header />
+    <SearchBar/>
+    {results && <Result/>}
     </div>
   );
 }
